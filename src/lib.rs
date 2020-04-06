5858a2a6669d1db1f5e7e8e0e52008afc7022253
Zach Shipko <zachshipko@gmail.com>
2020-04-06 09:27:26 -0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use ocaml::{FromValue, Pointer, Value};
+use ocaml::{FromValue, Opaque, Value};
 
 extern "C" fn finalize(value: Value) {
-    let mut ptr: Pointer<Vec<ocaml::Int>> = Pointer::from_value(value);
+    let mut ptr: Opaque<Vec<ocaml::Int>> = Opaque::from_value(value);
+    ptr.data_mut().clear();
     println!("{}", ptr.data().len());
     unsafe {
         std::ptr::drop_in_place(ptr.ptr_mut());
@@ -9,45 +10,45 @@ extern "C" fn finalize(value: Value) {
 }
 
 #[ocaml::func]
-pub fn vec_create(n: ocaml::Int) -> Pointer<'static, Vec<ocaml::Int>> {
-    let mut vec: Vec<ocaml::Int> = vec![0; n as usize];
-    let ptr = &mut vec as *mut Vec<_>;
+pub fn vec_create(n: ocaml::Int) -> Opaque<'static, Vec<ocaml::Int>> {
+    let vec: Vec<ocaml::Int> = Vec::with_capacity(n as usize);
+    let ptr = Opaque::new(&vec, Some(finalize));
     std::mem::forget(vec);
-    Pointer::new(ptr, Some(finalize))
+    ptr
 }
 
 #[ocaml::func]
-pub fn vec_length(handle: Pointer<Vec<ocaml::Int>>) -> ocaml::Int {
+pub fn vec_length(handle: Opaque<Vec<ocaml::Int>>) -> ocaml::Int {
     let p = handle.data();
     p.len() as ocaml::Int
 }
 
 #[ocaml::func]
-pub fn vec_push(mut handle: Pointer<Vec<ocaml::Int>>, x: ocaml::Int) {
+pub fn vec_push(mut handle: Opaque<Vec<ocaml::Int>>, x: ocaml::Int) {
     let p = handle.data_mut();
     p.push(x);
 }
 
 #[ocaml::func]
-pub fn vec_pop(mut handle: Pointer<Vec<ocaml::Int>>) -> Option<ocaml::Int> {
+pub fn vec_pop(mut handle: Opaque<Vec<ocaml::Int>>) -> Option<ocaml::Int> {
     let p = handle.data_mut();
     p.pop()
 }
 
 #[ocaml::func]
-pub fn vec_clear(mut handle: Pointer<Vec<ocaml::Int>>) {
+pub fn vec_clear(mut handle: Opaque<Vec<ocaml::Int>>) {
     let p = handle.data_mut();
     p.clear();
 }
 
 #[ocaml::func]
-pub fn vec_index(handle: Pointer<Vec<ocaml::Int>>, index: ocaml::Int) -> Option<ocaml::Int> {
+pub fn vec_index(handle: Opaque<Vec<ocaml::Int>>, index: ocaml::Int) -> Option<ocaml::Int> {
     let p = handle.data();
     p.get(index as usize).map(|x| *x)
 }
 
 #[ocaml::func]
-pub fn vec_set_index(mut handle: Pointer<Vec<ocaml::Int>>, index: ocaml::Int, x: ocaml::Int) {
+pub fn vec_set_index(mut handle: Opaque<Vec<ocaml::Int>>, index: ocaml::Int, x: ocaml::Int) {
     let p = handle.data_mut();
     p[index as usize] = x;
 }
